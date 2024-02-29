5f3496b5fe5a3931a276d0b90ffe7a84328d1b23
adrian1 <a.montesl.2020@alumnos.urjc.es>
2024-02-29 16:32:45 +0100

    basicWebApp

---
+++ webapp.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import socket
+
+class webApp:
+
+    def __init__(self, hostname, port):
+        "INICIAR NUESTRA APP WEB"
+        
+        # AF_INET = IPv4, SOCK_STREAM = TCP
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Habilitar reutilización de direcciones
+        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        mySocket.bind((hostname, port))
+
+        # Cola máxima de 5 conexiones TCP
+        mySocket.listen(5)
+
+        while True:
+            print("Esperando alguna conexión...")
+            connectionSocket, addr = mySocket.accept()
+            print("Conexión recibida de: " + str(addr))
+            recibido = connectionSocket.recv(2048)
+            print(f"El crudo en bytes{recibido}\n\n")
+
+            #Manejar/parsear recursos
+            parsed_request = self.parse(recibido.decode("utf-8"))
+            
+            #Proceso la peticion y creo la respuesta
+            return_code, html_respuesta = self.procces(parsed_request)
+
+            #Enviar respuesta
+            response = "HTTP/1.1 " + return_code + "\r\n\r\n" \
+                    + html_respuesta + "\r\n"
+            connectionSocket.send(response.encode("utf-8"))
+            connectionSocket.close()
+
+        
+
+    def parse(self, request):
+        "PARSEO LA PERICION EXTRAYENDO LA INFO"
+
+        print("PARSE: NO HAY NADA QUE PARSEAR AHORA MISMO")
+        return None
+    
+    def procces(self, parsed_request):
+        "PROCESA LOS DATOS DE LA PETICION"
+        "DEVUELVE EL CODIGO HTTP DE LA RESPUESTA Y UNA PAGINA HTML"
+
+        print("PROCCES: No HAY NADA QUE PROCESAR")
+        return "200 OK", "<html><body>Hola mundo</body></html>"
+    
+
+if __name__ == "__main__":
+    my_webapp = webApp("", 1234)
+    
